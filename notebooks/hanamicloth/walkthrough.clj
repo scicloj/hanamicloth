a7560f84e991ff598114c24cad32524f8c4d6a2f
daslu <daniel.slutsky@gmail.com>
2024-06-21 21:52:38 +0300

    walkthrough - WIP (switched datasets)

--- notebooks/hanamicloth/walkthrough.clj
+++ notebooks/hanamicloth/walkthrough.clj
@@ -2,8 +2,6 @@
 
 (ns hanamicloth.walkthrough
   (:require [scicloj.hanamicloth.v1.api :as hanami]
-            [scicloj.metamorph.ml.toydata :as toydata]
-            [scicloj.metamorph.ml.toydata.ggplot :as toydata.ggplot]
             [aerial.hanami.templates :as ht]
             [aerial.hanami.common :as hc]
             [tablecloth.api :as tc]
@@ -14,71 +12,113 @@
 :kindly/hide-code
 (def md (comp kindly/hide-code kind/md))
 
+;; ## Some datasets
+
+(defonce iris
+  (-> "https://vincentarelbundock.github.io/Rdatasets/csv/datasets/iris.csv"
+      (tc/dataset {:key-fn keyword})
+      (tc/rename-columns {:Sepal.Length :sepal-length
+                          :Sepal.Width :sepal-width
+                          :Petal.Length :petal-length
+                          :Petal.Width :petal-width
+                          :Species :species})))
+
+iris
+
+(defonce mtcars
+  (-> "https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv"
+      (tc/dataset {:key-fn keyword})))
+
+mtcars
+
 ;; ## Using the original Hanami templates & defaults
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base ht/point-chart
-                 {:X :sepal_width
-                  :Y :sepal_length
+                 {:X :sepal-width
+                  :Y :sepal-length
                   :MSIZE 200}))
 
 ;; ## Using Hanamicloth templates & defaults
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base hanami/point-chart
-                 #:hanami{:x :sepal_width
-                          :y :sepal_length
+                 #:hanami{:x :sepal-width
+                          :y :sepal-length
                           :mark-size 200}))
 
-(-> toydata.ggplot/mpg
+(-> iris
+    (hanami/base hanami/point-chart
+                 #:hanami{:x :sepal-width
+                          :y :sepal-length
+                          :color :species
+                          :mark-size 200}))
+
+(-> mtcars
+    (hanami/plot hanami/point-chart
+                 #:hanami{:x :mpg
+                          :y :disp
+                          :color :cyl
+                          :mark-size 200}))
+
+(-> mtcars
+    (hanami/plot hanami/point-chart
+                 #:hanami{:x :mpg
+                          :y :disp
+                          :color :cyl
+                          :color-type :nominal
+                          :mark-size 200}))
+
+(-> mtcars
     (hanami/plot hanami/boxplot-chart
                  #:hanami{:x :cyl
-                          :y :displ}))
+                          :x-type :nominal
+                          :y :disp}))
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base hanami/rule-chart
-                 #:hanami{:x :sepal_width
-                          :y :sepal_length
-                          :x2 :petal_width
-                          :y2 :petal_length
+                 #:hanami{:x :sepal-width
+                          :y :sepal-length
+                          :x2 :petal-width
+                          :y2 :petal-length
                           :mark-opacity 0.5
                           :mark-size 3
                           :color :species}))
 
 ;; ## Adding layers
 
-(-> (toydata/iris-ds)
-    (hanami/base #:hanami{:x :sepal_width
-                          :y :sepal_length
+(-> iris
+    (hanami/base #:hanami{:x :sepal-width
+                          :y :sepal-length
                           :mark-size 200})
     hanami/layer-point)
 
-(-> (toydata/iris-ds)
-    (hanami/layer-point #:hanami{:x :sepal_width
-                                 :y :sepal_length
+(-> iris
+    (hanami/layer-point #:hanami{:x :sepal-width
+                                 :y :sepal-length
                                  :mark-size 200}))
 
-(-> (toydata/iris-ds)
-    (hanami/base #:hanami{:x :sepal_width
-                          :y :sepal_length})
+(-> iris
+    (hanami/base #:hanami{:x :sepal-width
+                          :y :sepal-length})
     (hanami/layer-point #:hanami{:mark-size 200}))
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     (hanami/layer-line #:hanami{:mark-size 4
                                 :mark-color "brown"})
     (hanami/layer-point #:hanami{:mark-size 200}))
 
 ;; ## Updating data
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     (hanami/layer-line #:hanami{:mark-size 4
                                 :mark-color "brown"})
     (hanami/update-data tc/random 5)
@@ -86,11 +126,11 @@
 
 ;; ## Processing raw vega-lite
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     (hanami/layer-line #:hanami{:mark-size 4
                                 :mark-color "brown"})
     (hanami/layer-line {:MSIZE 4
@@ -102,65 +142,60 @@
 
 ;; ## Smoothing
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     hanami/layer-point
     (hanami/layer-smooth #:hanami{:mark-color "orange"}))
 
-(-> (toydata/iris-ds)
-    (hanami/base #:hanami{:x :sepal_width
-                          :y :sepal_length})
+(-> iris
+    (hanami/base #:hanami{:x :sepal-width
+                          :y :sepal-length})
     hanami/layer-point
-    (hanami/layer-smooth #:hanami{:predictors [:petal_width
-                                               :petal_length]}))
+    (hanami/layer-smooth #:hanami{:predictors [:petal-width
+                                               :petal-length]}))
 
 ;; ## Grouping
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
                           :color :species
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     hanami/layer-point
     hanami/layer-smooth)
 
-(-> (toydata/iris-ds)
+(-> iris
     (hanami/base #:hanami{:title "dummy"
                           :mark-color "green"
                           :color :species
                           :group []
-                          :x :sepal_width
-                          :y :sepal_length})
+                          :x :sepal-width
+                          :y :sepal-length})
     hanami/layer-point
     hanami/layer-smooth)
 
 ;; ## Example: out-of-sample predictions
 
-(-> (toydata/iris-ds)
-    (tc/concat (tc/dataset {:sepal_width (range 4 10)
-                            :sepal_length (repeat 6 nil)}))
+(-> iris
+    (tc/concat (tc/dataset {:sepal-width (range 4 10)
+                            :sepal-length (repeat 6 nil)}))
     (tc/map-columns :relative-time
-                    [:sepal_length]
+                    [:sepal-length]
                     #(if % "Past" "Future"))
-    (hanami/base #:hanami{:x :sepal_width
-                          :y :sepal_length
+    (hanami/base #:hanami{:x :sepal-width
+                          :y :sepal-length
                           :color "relative-time"
                           :group []})
     hanami/layer-point
     hanami/layer-smooth)
 
+;; ## Histograms
 
-
-;; (-> (toydata/iris-is)
-;;     (hanami/plot ht/rule-chart
-;;                {:X :sepal_width
-;;                 :Y :sepal_length
-;;                 :X2 :petal-width
-;;                 :Y2 :petal_length
-;;                 :OPACITY 0.2
-;;                 :SIZE 3
-;;                 :COLOR "species"}))
+(-> iris
+    (hanami/layer-histogram #:hanami{:x :sepal-width})
+    hanami/plot
+    (dissoc :encoding))
