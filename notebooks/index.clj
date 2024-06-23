d9fc8b1c1e447b4e153dd388136dcba339f7b8bd
daslu <daniel.slutsky@gmail.com>
2024-06-23 23:08:22 +0300

    index page - added goals

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -43,7 +43,18 @@ April 2024 talk at London Clojurians:
 
 (md "
 
-Hanamicloth is a composition of Hanami data visualizations and Tablecloth datasets
+## Goals
+
+- Have a functional grammar for common plotting tasks (but not all tasks).
+- In particular, provide an easy way to work with layers.
+- By default, infer relevant information from the data (e.g., field types).
+- Catch common errors using the data (e.g., missing fields).
+- Be able to use backend Clojure for relevant statistical tasks (e.g., smoothing by regression, histograms, density estimation).
+- Be able to Rely on Vega-Lite for other some components of the pipeline (e.g., scales and coordinates).
+- Provide simpler Hanami templates, compared to the original ones.
+- Still have the option of using the original Hanami templates.
+- Still be able to use all of [Vega-Lite](https://vega.github.io/vega-lite/) in its raw format for the highest flexibility.
+
 
 ## Discussion
 - development - topic threads under [#hanamicloth-dev](https://clojurians.zulipchat.com/#narrow/stream/443101-hanamicloth-dev) at the [Clojurians Zulip chat](https://scicloj.github.io/docs/community/chat/) or [Github Issues](https://github.com/scicloj/hanamicloth/issues)
